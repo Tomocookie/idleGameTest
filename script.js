2e11f336d0662e4d8dea2dc2944c329df02aa331
Tomocookie <tomo20060303@gmail.com>
2023-11-13 14:54:46 +0900

    cookieテスト

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
+// カウントの初期値
 let count = 0;
 
+// カウントの要素を取得
 const counterElement = document.getElementById('counter');
+// カウントボタンの要素を取得
 const clickButton = document.getElementById('clickButton');
 
-clickButton.addEventListener('click', () => {
+// ページが読み込まれたときにセーブデータをロードする
+window.onload = function() {
+    loadSaveData();
+};
+
+// ボタンがクリックされたときの処理
+clickButton.addEventListener('click', function() {
     count++;
     counterElement.textContent = count;
+    saveData();
 });
+
+// セーブデータをCookieに保存する関数
+function saveData() {
+    document.cookie = `count=${count}; expires=${new Date(2023, 11, 31).toUTCString()}; path=/`;
+}
+
+// Cookieからセーブデータを読み込む関数
+function loadSaveData() {
+    const cookieValue = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('count='))
+        .split('=')[1];
+
+    if (cookieValue) {
+        count = parseInt(cookieValue);
+        counterElement.textContent = count;
+    }
+}
